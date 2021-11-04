f0657def5ed8dd3e223dde79a98b4bfcaa38092a
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-11-04 14:55:58 -0500

    beginning js

---
+++ script.js
@@ -0,0 +1,12 @@
+let images = [
+    "img/dice-six-faces-one.svg",
+    "img/dice-six-faces-two.svg",
+    "img/dice-six-faces-three.svg",
+    "img/dice-six-faces-four.svg",
+    "img/dice-six-faces-five.svg",
+    "img/dice-six-faces-six.svg"
+];
+
+let dice = document.querySelectorAll("img");
+
+// console.log(dice);
