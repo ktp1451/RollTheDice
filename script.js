ee5ffe5c2c93d7baad88d5078138c869785bca33
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-11-04 15:13:15 -0500

    update js for dice rolling

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function roll () {
     dice.forEach(function(die){
         die.classList.add("shake");
     });
+    let dieOneValue = Math.floor(Math.random()*6);
+    let dieTwoValue = Math.floor(Math.random()*6);
+    console.log(dieOneValue,dieTwoValue);
+    document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+    document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+    document.querySelector("#total").innerHTML = "Your roll is " + ( (dieOneValue +1) + (dieTwoValue + 1) );
+
+    1000
+    ;
 }
+roll();
