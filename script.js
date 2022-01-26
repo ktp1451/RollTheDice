693f24b15d63fef198268927a0b6886d00d2b995
Michelle Berta <bertafamily@sbcglobal.net>
2022-01-26 17:09:53 -0600

    added modal

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-let images = [
-  "img/dice-six-faces-one.svg",
-  "img/dice-six-faces-two.svg",
-  "img/dice-six-faces-three.svg",
-  "img/dice-six-faces-four.svg",
-  "img/dice-six-faces-five.svg",
-  "img/dice-six-faces-six.svg"
-];
+// modal
+$(document).ready(function () {
+  $(".modal").modal();
+});
+
+let images = ["img/dice-six-faces-one.svg", "img/dice-six-faces-two.svg", "img/dice-six-faces-three.svg", "img/dice-six-faces-four.svg", "img/dice-six-faces-five.svg", "img/dice-six-faces-six.svg"];
 
 let dice = document.querySelectorAll("img");
 
 // console.log(dice);
 
-
 // Dice icons shake when button is clicked
-$('#shake').click(function() {
-shake($('.diceIcon'));
+$("#shake").click(function () {
+  shake($(".diceIcon"));
 });
 
-
-// adaptable SHAKE function, from 
-// https://bradleyhamilton.com/projects/shake/index.html 
+// adaptable SHAKE function, from
+// https://bradleyhamilton.com/projects/shake/index.html
 function shake(thing) {
-var interval = 100;
-var distance = 10;
-var times = 6;
+  var interval = 100;
+  var distance = 10;
+  var times = 6;
 
-for (var i = 0; i < (times + 1); i++) {
-  $(thing).animate({
-    left:
-      (i % 2 == 0 ? distance : distance * -1)
-  }, interval);
-}
-$(thing).animate({
-  left: 0,
-  top: 0
-}, interval);
+  for (var i = 0; i < times + 1; i++) {
+    $(thing).animate(
+      {
+        left: i % 2 == 0 ? distance : distance * -1,
+      },
+      interval
+    );
+  }
+  $(thing).animate(
+    {
+      left: 0,
+      top: 0,
+    },
+    interval
+  );
 }
 // end SHAKE
-
-
-
 
 function roll() {
   dice.forEach(function (die) {
@@ -52,23 +50,7 @@ function roll() {
     console.log(dieOneValue, dieTwoValue);
     document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
     document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-    document.querySelector("#total").innerHTML =
-      "Your roll is " + (dieOneValue + 1 + (dieTwoValue + 1));
+    document.querySelector("#total").innerHTML = "Your roll is " + (dieOneValue + 1 + (dieTwoValue + 1));
   }, 1000);
 }
 roll();
-
-const popup = document.querySelector('.popup');
-const close = document.querySelector('.close');
-
-window.onload = function(){
-  setTimeout(function(){
-    popup.style.display = "block"
-
-    //Add some time delay to show popup
-  }, 2000)
-}
-
-close.addEventListener('click', () => {
-  popup.style.display = "none";
-})
